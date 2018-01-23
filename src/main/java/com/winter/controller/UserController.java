7967932ed2c92b97df7875f1111d355665e5b023
elevencityhost <15901506742@163.com>
2018-01-23 17:40:57 +0800

    use thymeleaf to resolve web pages

--- src/main/java/com/winter/controller/UserController.java
+++ src/main/java/com/winter/controller/UserController.java
@@ -4,6 +4,7 @@ import com.winter.model.User;
 import com.winter.service.UserService;
 import com.winter.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
  * @author 十一城城主
  * @data 2018/1/22 11:20
  */
-@RestController
+@Controller
 @RequestMapping(value = "/user")
 public class UserController {
 
@@ -20,12 +21,19 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/add")
+    @ResponseBody
     public int addUser(User user){
         return userService.addUser(user);
     }
 
     @GetMapping(value = "/all/{pageNum}/{pageSize}")
+    @ResponseBody
     public List<User> findAllUser(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize){
         return userService.findAllUser(pageNum,pageSize);
+    }
+
+    @RequestMapping(value = "/index")
+    public String login(){
+        return "/index";
     }
 }
