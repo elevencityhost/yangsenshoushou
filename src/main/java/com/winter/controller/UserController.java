d5463546625db5b3393ebb74a7a9f513f14be76b
elevencityhost <15901506742@163.com>
2018-03-05 11:43:59 +0800

    空格

--- src/main/java/com/winter/controller/UserController.java
+++ src/main/java/com/winter/controller/UserController.java
@@ -61,7 +61,7 @@ public class UserController {
         System.out.println(user);
        return userService.updateUser(user);
     }
-
+    
     @ApiIgnore
     @RequestMapping(value = "/index")
     public String login(Model model){
